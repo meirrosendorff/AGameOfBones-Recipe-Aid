2dac2ef583f0b36736659f21d7010316d5eaa4ff
meirrosendorff <meir.rosendorff@gmail.com>
2019-05-20 11:11:39 +0200

    Added Tab Bar Controller for navigation

---
+++ RecipeAid/RecipeAid/SearchAndAdd/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  RecipeAid
+//
+//  Created by Meir Rosendorff on 2019/05/20.
+//  Copyright © 2019 Meir Rosendorff. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Add Recipe"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
