2c8f5ba35691c1940615e9406eeefb6f7ad0496d
meirrosendorff <meir.rosendorff@gmail.com>
2019-05-22 07:31:14 +0200

    Created and began UI of Add Recipe Page

--- RecipeAid/RecipeAid/SearchAndAdd/AddViewController.swift
+++ RecipeAid/RecipeAid/SearchAndAdd/AddViewController.swift
@@ -8,23 +8,57 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+  
+  
+  @IBOutlet weak var weekPicker: UIPickerView!
+  
+  
+
+  @IBOutlet weak var dayPicker: UIPickerView!
+  
+  
+  let weeks: [String] = ["Christmas Break", "Jan Week 1"]
+  let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday", "Sunday"]
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    if pickerView.tag == 1 { //Week picker
+      return weeks.count
+    }else if pickerView.tag == 2{ //Day picker
+      return days.count
+    }
+    return 0
+  }
+  
+  func pickerView(_ pickerView: UIPickerView,
+                  titleForRow row: Int,
+                  forComponent component: Int) -> String? {
+
+    if pickerView.tag == 1 { //Week picker
+      return weeks[row]
+    }else if pickerView.tag == 2{
+      return days[row]
+    }
+    return ""
+
+  }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "Add Recipe"
+      title = "Add"
+      
+      dayPicker.dataSource = self
+      dayPicker.delegate = self
+      
+      weekPicker.dataSource = self
+      weekPicker.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
