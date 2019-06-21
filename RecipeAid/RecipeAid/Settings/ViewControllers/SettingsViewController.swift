96ec5f6feeeae475bec736ab4cc0a531d670df80
meirrosendorff <meir.rosendorff@gmail.com>
2019-06-21 12:52:31 +0200

    MVVMed the Settings page

--- RecipeAid/RecipeAid/Settings/ViewControllers/SettingsViewController.swift
+++ RecipeAid/RecipeAid/Settings/ViewControllers/SettingsViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var maxTimeTextBox: UITextField!
   @IBOutlet weak var minTimeTextBox: UITextField!
   @IBOutlet weak var maxCaloriesTextBox: UITextField!
   @IBOutlet weak var minCaloriesTextBox: UITextField!
-  @IBOutlet weak var emailAdressLabel: UILabel!
+  @IBOutlet weak var emailAddressLabel: UILabel!
   @IBOutlet weak var usernameLabel: UILabel!
   @IBOutlet weak var caloriesLabel: UILabel!
   @IBOutlet weak var caloriesColonLabel: UILabel!
@@ -31,24 +32,66 @@ class SettingsViewController: UIViewController {
   @IBOutlet weak var logoutButton: UIButton!
   var gradientLayer: GradientLayer?
   let formatter = Formatter()
-  let settings = [("High Protein", false), ("Low Fat", false),
-                  ("Low Carb", false), ("Low Suger", false),
-                  ("Vegetarian", false), ("Vegan", false),
-                  ("Alcohol Free", false), ("Nut Free", false)]
+  var viewModel: SettingsViewModelProtocol!
+  var savedAlert: UIAlertController!
+
   override func viewDidLoad() {
+
     super.viewDidLoad()
+    viewModel = SettingsViewModel()
     gradientLayer = GradientLayer(view: view)
     gradientLayer?.addGradientToView()
     formatViews()
+    hideKeyboardWhenTappedAround()
     self.title = "Settings"
+    setUserDetails()
+    addUsersCurrentSettings()
+    setUpSavedAlert()
   }
+
+  private func setUpSavedAlert() {
+    self.savedAlert = UIAlertController(
+      title: "Saved", message: "Settings Succesfully Saved", preferredStyle: UIAlertController.Style.alert)
+  }
+
+  @objc
+  private func dismissAlert() {
+    self.savedAlert.dismiss(animated: true, completion: nil)
+  }
+
+  private func loadSettingsToView() {
+
+    setUserDetails()
+    addUsersCurrentSettings()
+    collectionView.reloadData()
+  }
+
+  private func setUserDetails() {
+
+    profilePicImageView.image = UIImage(named: viewModel.profilePic)
+    usernameLabel.text = viewModel.userName
+    emailAddressLabel.text = viewModel.emailAddress
+  }
+
+  private func addUsersCurrentSettings() {
+
+    minCaloriesTextBox.text = viewModel.minCalories
+    maxCaloriesTextBox.text = viewModel.maxCalories
+    minTimeTextBox.text = viewModel.minTime
+    maxTimeTextBox.text = viewModel.maxTime
+    dietryRestrictionsTextBox.text = viewModel.unwantedFoods
+  }
+
   override func viewWillLayoutSubviews() {
+
     super.viewWillLayoutSubviews()
     if let gradientLayer = gradientLayer {
       gradientLayer.updateBounds()
     }
   }
+
   func formatViews() {
+
     formatter.formatLabelAsMainText(usernameLabel, ofSize: 20)
     formatter.formatLabelAsMainText(caloriesLabel, ofSize: 17)
     formatter.formatLabelAsMainText(caloriesColonLabel, ofSize: 17)
@@ -57,7 +100,7 @@ class SettingsViewController: UIViewController {
     formatter.formatLabelAsMainText(timeColonLabel, ofSize: 17)
     formatter.formatLabelAsMainText(dietryRestrictionsLabel, ofSize: 17)
     formatter.formatLabelAsMainText(dietryRestrictionsInstructionLabel, ofSize: 12)
-    formatter.formatLabelAsSubtext(emailAdressLabel, ofSize: 17)
+    formatter.formatLabelAsSubtext(emailAddressLabel, ofSize: 17)
     formatter.formatTextField(maxCaloriesTextBox, ofSize: 14)
     formatter.formatTextField(minCaloriesTextBox, ofSize: 14)
     formatter.formatTextField(maxTimeTextBox, ofSize: 14)
@@ -68,6 +111,51 @@ class SettingsViewController: UIViewController {
     caloriesDashLabel.textColor = formatter.getButtonBorderColor()
     timeDashLabel.textColor = formatter.getButtonBorderColor()
   }
+
+  @IBAction func saveSettings(_ sender: Any) {
+
+    if !addSettingsToViewModel() { return }
+    viewModel.save()
+    loadSettingsToView()
+    self.present(savedAlert, animated: true, completion: nil)
+    Timer.scheduledTimer(
+      timeInterval: 0.75, target: self, selector: #selector(dismissAlert), userInfo: nil, repeats: false)
+  }
+
+  private func addSettingsToViewModel() -> Bool {
+
+    do {
+      try viewModel.setCalories(min: minCaloriesTextBox.text ?? "", max: maxCaloriesTextBox.text ?? "")
+    } catch {
+      displayErrorAlert(error: error, forLabel: "Calories")
+      return false
+    }
+
+    do {
+      try viewModel.setTimes(min: minTimeTextBox.text ?? "", max: maxTimeTextBox.text ?? "")
+    } catch {
+      displayErrorAlert(error: error, forLabel: "Time")
+      return false
+    }
+
+    viewModel.setUnwatedFoods(foods: dietryRestrictionsTextBox.text ?? "")
+    return true
+  }
+
+  private func displayErrorAlert(error: Error, forLabel label: String) {
+
+    let alert = UIAlertController(title: "Error Saving", message: "", preferredStyle: UIAlertController.Style.alert)
+    switch error {
+    case RecipeError.invalidNumberString:
+      alert.message = "\(label) must be a number!"
+    case RecipeError.minMaxError:
+      alert.message = "minimum \(label) must be less than maximum"
+    default:
+      alert.message = "Problem saving \(label)"
+    }
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    self.present(alert, animated: true, completion: nil)
+  }
 }
 
 typealias CollectionView = UICollectionViewDelegate
@@ -75,29 +163,38 @@ typealias CollectionView = UICollectionViewDelegate
   & UICollectionViewDelegateFlowLayout
 
 extension SettingsViewController: CollectionView {
+
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return settings.count
+
+    return viewModel.numRestrictions
   }
+
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(
-      withReuseIdentifier: "dietryRestrictions",
+
+    guard let cell = collectionView.dequeueReusableCell( withReuseIdentifier: "dietryRestrictions",
       for: indexPath)
       as? DietryRestrictionsCollectionViewCell else {
+
       return UICollectionViewCell()
     }
-    let (name, isChosen) = settings[indexPath.row]
-    cell.setup(name, isChosen)
+
+    cell.setup(viewModel: viewModel, index: indexPath.row)
+
     cell.layer.borderColor = formatter.getFillColor().cgColor
     cell.layer.borderWidth = 2
     cell.backgroundColor = formatter.getFillColor().withAlphaComponent(0.8)
+
     return cell
   }
+
   func collectionView(
     _ collectionView: UICollectionView,
     layout collectionViewLayout: UICollectionViewLayout,
     sizeForItemAt indexPath: IndexPath) -> CGSize {
+
     return CGSize(width: (view.frame.width - 20) / 2, height: 60)
   }
+
   override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
     //Ask PJ about why the app crashes on rotation if this is not optionally accessed
     //it only crashes before the view is innialized
---
+++ RecipeAid/RecipeAid/Settings/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,159 @@
+//
+//  SettingsViewModel.swift
+//  RecipeAid
+//
+//  Created by Meir Rosendorff on 2019/06/21.
+//  Copyright © 2019 Meir Rosendorff. All rights reserved.
+//
+
+import Foundation
+
+class SettingsViewModel: SettingsViewModelProtocol {
+
+  var profilePic: String
+  var userName: String
+  var emailAddress: String
+  var minCalories: String { return minCaloriesInt > 0 ? String(minCaloriesInt) : "" }
+  var maxCalories: String { return maxCaloriesInt > 0 ? String(maxCaloriesInt) : ""  }
+  var minTime: String { return minTimeInt > 0 ? String(minTimeInt) : "" }
+  var maxTime: String { return maxTimeInt > 0 ? String(maxTimeInt) : "" }
+  var unwantedFoods: String
+  var restrictions: [(String, Bool)]
+  private var minCaloriesInt: Int
+  private var maxCaloriesInt: Int
+  private var minTimeInt: Int
+  private var maxTimeInt: Int
+
+  var numRestrictions: Int { return restrictions.count }
+
+  init() {
+
+    profilePic = ""
+    userName = ""
+    emailAddress = ""
+    minCaloriesInt = 0
+    maxCaloriesInt = 0
+    minTimeInt = 0
+    maxTimeInt = 0
+    unwantedFoods = ""
+    restrictions = []
+
+    setProfileData()
+    updateSettingsFromUserDefaults()
+  }
+
+  private func setProfileData() {
+    //not sure where this data will be fetched from yet
+    profilePic = "background"
+    userName = "User Name"
+    emailAddress = "user.name@emailadress.com"
+  }
+
+  private func getRestrictionsFromUserDefaults() {
+
+    let dict =
+      UserDefaults.standard.object(forKey: "restrictionOptionsArray") as? [String: Bool] ?? [String: Bool]()
+
+    for (name, isSelected) in dict {
+      restrictions.append((name, isSelected))
+    }
+  }
+
+  private func getCaloriesFromUserDefaults() {
+
+    minCaloriesInt = UserDefaults.standard.integer(forKey: "minCalories")
+    maxCaloriesInt = UserDefaults.standard.integer(forKey: "maxCalories")
+  }
+
+  private func getTimesFromUserDefaults() {
+
+    minTimeInt = UserDefaults.standard.integer(forKey: "minTime")
+    maxTimeInt = UserDefaults.standard.integer(forKey: "maxTime")
+  }
+
+  private func getUnwantedFoodsFromUserDefaults() {
+
+    let foodArray = UserDefaults.standard.object(forKey: "excludedFoodsArray") as? [String] ?? []
+
+    unwantedFoods = foodArray.joined(separator: ", ")
+  }
+
+  func restrictionName(at pos: Int) -> String {
+    return restrictions[pos].0
+  }
+
+  func restrictionIsSelected(at pos: Int) -> Bool {
+    return restrictions[pos].1
+  }
+
+  func selectRestriction(at pos: Int) {
+    restrictions[pos].1 = true
+  }
+
+  func deselectRestriction(at pos: Int) {
+    restrictions[pos].1 = false
+  }
+
+  private func getValidNumbers(min: String, max: String) throws -> (Int, Int) {
+
+    guard let minAttempt = Double(min != "" ? min : "0") else {
+      throw RecipeError.invalidNumberString("min Calories not a number")
+    }
+
+    guard let maxAttempt = Double(max != "" ? max : "0") else {
+      throw RecipeError.invalidNumberString("max Calories not a number")
+    }
+
+    if minAttempt > maxAttempt {
+      throw RecipeError.minMaxError("min calories greater than max calories")
+    }
+
+    return (Int(minAttempt), Int(maxAttempt))
+  }
+
+  func setCalories(min: String, max: String) throws {
+
+    let values = try getValidNumbers(min: min, max: max)
+
+    minCaloriesInt = values.0
+    maxCaloriesInt = values.1
+  }
+
+  func setTimes(min: String, max: String) throws {
+
+    let values = try getValidNumbers(min: min, max: max)
+
+    minTimeInt = values.0
+    maxTimeInt = values.1
+  }
+
+  func setUnwatedFoods(foods: String) {
+    unwantedFoods = foods
+  }
+
+  func save() {
+
+    let restrictionsToSave = restrictions.reduce(into: [:]) { $0[$1.0] = $1.1 }
+    UserDefaults.standard.set(restrictionsToSave, forKey: "restrictionOptionsArray")
+
+    UserDefaults.standard.set(minCaloriesInt, forKey: "minCalories")
+    UserDefaults.standard.set(maxCaloriesInt, forKey: "maxCalories")
+    UserDefaults.standard.set(minTimeInt, forKey: "minTime")
+    UserDefaults.standard.set(maxTimeInt, forKey: "maxTime")
+
+    let excludedFoodsArray = unwantedFoods.split(separator: ",").map {
+      $0.trimmingCharacters(in: .whitespaces)
+    }
+    UserDefaults.standard.set(excludedFoodsArray, forKey: "excludedFoodsArray")
+
+    updateSettingsFromUserDefaults()
+  }
+
+  private func updateSettingsFromUserDefaults() {
+
+    getRestrictionsFromUserDefaults()
+    getCaloriesFromUserDefaults()
+    getTimesFromUserDefaults()
+    getUnwantedFoodsFromUserDefaults()
+  }
+}
